9def29056587b66a1582e949bc7e657e1b8e4a10
Kristjan Pšennõi <kristjan.psennoi@hotmail.com>
2021-12-04 22:14:44 +0200

    Formatted code

--- src/kata/FizzBuzz.java
+++ src/kata/FizzBuzz.java
@@ -2,11 +2,14 @@ package kata;
 
 public class FizzBuzz {
 
-	  public static String getResult(int number) {
-		  if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
-		  else if (number % 3 == 0) return "Fizz";
-		  else if (number % 5 == 0) return "Buzz";
-		  return Integer.toString(number);
-	  }
+	public static String getResult(int number) {
+		if (number % 3 == 0 && number % 5 == 0)
+			return "FizzBuzz";
+		else if (number % 3 == 0)
+			return "Fizz";
+		else if (number % 5 == 0)
+			return "Buzz";
+		return Integer.toString(number);
+	}
 
 }
