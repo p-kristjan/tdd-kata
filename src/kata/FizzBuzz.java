795540d3e05814f70135379dd694e72de0b3eb3e
Kristjan Pšennõi <kristjan.psennoi@hotmail.com>
2021-12-04 22:06:08 +0200

    Test shouldReturnFizzIfNumberDivisibleBy5 - Pass

--- src/kata/FizzBuzz.java
+++ src/kata/FizzBuzz.java
@@ -4,6 +4,7 @@ public class FizzBuzz {
 
 	  public static String getResult(int number) {
 		  if (number % 3 == 0) return "Fizz";
+		  else if (number % 5 == 0) return "Buzz";
 		  return Integer.toString(number);
 	  }
 
