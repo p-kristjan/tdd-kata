647eac8ce359a48e08dd2c466fe2e6c1859ca472
Kristjan Pšennõi <kristjan.psennoi@hotmail.com>
2021-12-04 21:57:20 +0200

    Test shouldReturnFizzIfNumberDivisibleBy3 - Fail

--- src/kata/FizzBuzzTest.java
+++ src/kata/FizzBuzzTest.java
@@ -7,10 +7,16 @@ import org.junit.jupiter.api.Test;
 public class FizzBuzzTest {
 
 	  @Test
-	  public void shouldReturnNumbers0To100() {
-	    assertEquals("2", FizzBuzz.getResult(2));
-	    assertEquals("4", FizzBuzz.getResult(4));
-	    assertEquals("7", FizzBuzz.getResult(7));
+	  public void shouldReturnNumbers0To100IfNoOtherRequirementIsFulfilled() {
+		  assertEquals("2", FizzBuzz.getResult(2));
+		  assertEquals("4", FizzBuzz.getResult(4));
+		  assertEquals("7", FizzBuzz.getResult(7));
 	  }
 	
+	  @Test
+	  public void shouldReturnFizzIfNumberDivisibleBy3() {
+		  assertEquals("Fizz", FizzBuzz.getResult(3));
+		  assertEquals("Fizz", FizzBuzz.getResult(6));
+		  assertEquals("Fizz", FizzBuzz.getResult(9));
+	  }
 }
