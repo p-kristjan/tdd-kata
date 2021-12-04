f24bb0bc78ededd93e1c482ee23ab6f9820bd2d2
Kristjan Pšennõi <kristjan.psennoi@hotmail.com>
2021-12-04 21:53:17 +0200

    Test shouldReturnNumbers0To100 - Fail

--- src/kata/FizzBuzz.java
+++ src/kata/FizzBuzz.java
@@ -2,4 +2,9 @@ package kata;
 
 public class FizzBuzz {
 
+	public static Object getResult(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
--- src/kata/FizzBuzzTest.java
+++ src/kata/FizzBuzzTest.java
@@ -1,5 +1,16 @@
 package kata;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 public class FizzBuzzTest {
 
+	  @Test
+	  public void shouldReturnNumbers0To100() {
+	    assertEquals("2", FizzBuzz.getResult(2));
+	    assertEquals("4", FizzBuzz.getResult(4));
+	    assertEquals("7", FizzBuzz.getResult(7));
+	  }
+	
 }
